b204af5a7eb62d2b8ba35f9d5b528d423e77b4bc
James <james.jcarline@gmail.com>
2023-01-05 22:20:44 +0000

    Read CSV line by line and output

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
-contents = File.read('event_attendees.csv')
-puts contents
+lines = File.readlines('event_attendees.csv')
+lines.each do |line|
+  columns = line.split(',')
+  p columns
+end
