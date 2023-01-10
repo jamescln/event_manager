1d2af6720cbd0217d04f77e09ccf3c156bbed5a9
James <james.jcarline@gmail.com>
2023-01-10 18:55:56 +0000

    Use the CSV library to parse the file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,24 @@
 # frozen_string_literal: true
 
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
+require 'csv'
+puts 'EventManager Initialised.'
+
+# lines = File.readlines('event_attendees.csv')
+# lines.each_with_index do |line, index|
   # use next to skip an iteration based on an evaluation
-  next if index.zero?
+  # next if index.zero?
 
   # split converts a string to an array
   # pass a comma as an argument so that the array
   # is split on that character
-  columns = line.split(',')
+  # columns = line.split(',')
   # the data can now be referenced with an index
-  name = columns[2]
+  # name = columns[2]
+  # p name
+# end
+
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   p name
 end
