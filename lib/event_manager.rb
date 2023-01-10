753c3af5d2e2687f828fc6b4f1c025754ba1e0d7
James <james.jcarline@gmail.com>
2023-01-10 19:25:03 +0000

    Use symbol conversions for headers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,6 +7,7 @@ contents = CSV.open('event_attendees.csv',
                     headers: true,
                     header_converters: :symbol)
 contents.each do |row|
-  name = row[2]
-  p name
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
