6c95c03900c67f7bbfe5f7465b3747fc6b87bd92
James <james.jcarline@gmail.com>
2023-01-10 18:41:19 +0000

    Add documentation, skip CSV header

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 lines = File.readlines('event_attendees.csv')
-lines.each do |line|
+lines.each_with_index do |line, index|
+  # use next to skip an iteration based on an evaluation
+  next if index.zero?
+
+  # split converts a string to an array
+  # pass a comma as an argument so that the array
+  # is split on that character
   columns = line.split(',')
+  # the data can now be referenced with an index
   name = columns[2]
   p name
 end
