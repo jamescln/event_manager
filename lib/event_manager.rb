dc539944b5c36c6962b0b0de7c1fda3544229e3d
James <james.jcarline@gmail.com>
2023-01-10 19:00:09 +0000

    Make rubocop happy

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -3,21 +3,9 @@
 require 'csv'
 puts 'EventManager Initialised.'
 
-# lines = File.readlines('event_attendees.csv')
-# lines.each_with_index do |line, index|
-  # use next to skip an iteration based on an evaluation
-  # next if index.zero?
-
-  # split converts a string to an array
-  # pass a comma as an argument so that the array
-  # is split on that character
-  # columns = line.split(',')
-  # the data can now be referenced with an index
-  # name = columns[2]
-  # p name
-# end
-
-contents = CSV.open('event_attendees.csv', headers: true)
+contents = CSV.open('event_attendees.csv',
+                    headers: true,
+                    header_converters: :symbol)
 contents.each do |row|
   name = row[2]
   p name
