704dac326d3b39ecccef07da92a2bc6979e5efc2
James <james.jcarline@gmail.com>
2023-01-17 20:52:14 +0000

    Return legislators for each address. Add exception handling

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyAgcTcdrxeK_jjRL_K65d-OVCPBUM8QxDU'
 
 def clean_zipcode(zip)
   zip.to_s.rjust(5, '0')[0..4]
@@ -15,5 +19,17 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+    legislator_names = legislators.map(&:name)
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
